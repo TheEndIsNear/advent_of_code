2fc91db55253c13ca9c15e672cbfed8542413224
Matt Willy <matt.willy@gmail.com>
2022-12-01 15:50:54 -0700

    solve day 2 problem 2

--- 2020/day2/lib/day2.ex
+++ 2020/day2/lib/day2.ex
@@ -1,4 +1,5 @@
 defmodule Day2 do
+  @spec solve() :: integer()
   def solve do
     read_input()
     |> Stream.map(&parse_line/1)
@@ -6,7 +7,8 @@ defmodule Day2 do
     |> Enum.count()
   end
 
-  def parse_line(line) do
+  @spec parse_line((String.t())) :: map()
+  defp parse_line(line) do
     [range, char, password] = String.split(line)
 
     [low, high] = String.split(range, "-") |> Enum.map(&String.to_integer/1)
@@ -16,7 +18,8 @@ defmodule Day2 do
     %{low: low, high: high, char: char, password: password}
   end
 
-  def validate_password(%{password: password, char: char, low: low, high: high}) do
+  @spec validate_password(map()) :: boolean
+  defp validate_password(%{password: password, char: char, low: low, high: high}) do
     char_count =
       password
       |> Enum.filter(&(&1 == char))
@@ -25,6 +28,7 @@ defmodule Day2 do
     is_valid?(char_count, low, high)
   end
 
+  @spec is_valid?(non_neg_integer(), non_neg_integer(), non_neg_integer()) :: integer()
   defp is_valid?(char_count, low, high) when char_count >= low and char_count <= high, do: true
   defp is_valid?(_, _, _), do: false
 
